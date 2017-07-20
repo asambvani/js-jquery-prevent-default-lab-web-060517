e07e3d324283b3d5a45e3dca29ef412ace21611b
asambvani <alexsambvani@gmail.com>
2017-07-20 15:30:18 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
+  addUl();
 });
 
 // define functions here
+function addUl(){
+  ul = document.createElement("ul");
+  ul.id = "list";
+  $('h3')[0].appendChild(ul);
+}
+function submitForm(){
+
+  $('form').on('submit',()=>{
+    event.preventDefault();
+    li = document.createElement("li");
+    li.innerHTML = $('input#item').val();
+    $('#list')[0].appendChild(li);
+
+
+  });
+}
